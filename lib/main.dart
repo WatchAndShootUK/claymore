009944c400d3d5a881a8b260526b7d6e94d1acec
Mark Wileman <wasd@Marks-MacBook-Pro.local>
2026-06-14 23:57:38 +0100

    Deployment

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:claymore/models/user.dart';
 import 'package:claymore/pages/home_page.dart';
+import 'package:claymore/pages/login_page.dart';
 import 'package:claymore/services/firestore_service.dart';
 import 'package:claymore/state/app_data.dart';
 import 'package:flutter/material.dart';
@@ -31,9 +32,8 @@ class MainApp extends StatelessWidget {
         ),
       );
     }
-    appData.currentUser = appData.users[1];
     return MaterialApp(
-      home: HomePage(),
+      home: LoginPage(),
       theme: ThemeData(
         brightness: Brightness.dark,
         colorScheme: const ColorScheme.dark(
